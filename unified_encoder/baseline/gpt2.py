c028b4a66404c2c6ecf89cc4bdab6cd99dbf3581
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2022-02-12 15:31:14 -0500

    New: train logging

--- unified_encoder/baseline/gpt2.py
+++ unified_encoder/baseline/gpt2.py
@@ -1,4 +1,4 @@
-from typing import List, Callable
+from typing import List, Dict, Callable
 
 import numpy as np
 import transformers
@@ -6,6 +6,7 @@ import transformers
 from transformers import AutoTokenizer, AutoModelWithLMHead, AutoConfig, GPT2LMHeadModel, GPT2TokenizerFast
 from transformers import Trainer, TrainingArguments, SchedulerType, TrainerCallback
 from transformers.training_args import OptimizerNames
+
 from transformers import DataCollatorForLanguageModeling, default_data_collator
 from datasets import load_dataset, Dataset
 from datasets import load_metric
@@ -62,7 +63,7 @@ def get_model_n_tokenizer(name='gpt2') -> tuple[GPT2LMHeadModel, GPT2TokenizerFa
 def get_train_setup(name='gpt2') -> tuple[TrainingArguments, DataCollatorForLanguageModeling]:
     D_TRAIN_ARGS = {
         'debug': dict(
-            learning_rate=5e-2,
+            learning_rate=5e-4,
             batch_size=4,
             weight_decay=1e-2,
             num_train_epochs=4,
@@ -86,7 +87,7 @@ def get_train_setup(name='gpt2') -> tuple[TrainingArguments, DataCollatorForLang
     lr, bsz, decay, n_ep, sch = (D_TRAIN_ARGS[name][k] for k in [
         'learning_rate', 'batch_size', 'weight_decay', 'num_train_epochs', 'lr_scheduler_type'
     ])
-    ic(n_ep)
+    # ic(n_ep)
 
     return TrainingArguments(
         output_dir=os.path.join(PATH_BASE, DIR_PROJ, DIR_MDL, 'gpt2'),
@@ -96,6 +97,7 @@ def get_train_setup(name='gpt2') -> tuple[TrainingArguments, DataCollatorForLang
         per_device_eval_batch_size=bsz,
         # Adam's beta1, beta2, epsilon taken from
         # https://github.com/huggingface/transformers/blob/master/examples/pytorch/language-modeling/run_clm.py
+        learning_rate=lr,
         adam_beta1=0.9,
         adam_beta2=0.999,
         adam_epsilon=1e-08,
@@ -106,26 +108,192 @@ def get_train_setup(name='gpt2') -> tuple[TrainingArguments, DataCollatorForLang
         logging_strategy='steps',
         logging_steps=1,
         fp16=torch.cuda.is_available(),  # TODO: dynamic loss scaling??
-        optim=OptimizerNames.ADAMW_TORCH
+        optim=OptimizerNames.ADAMW_TORCH,
+        disable_tqdm=True
     ), DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
 
-class PrinterCallback(TrainerCallback):
-    def on_log(self, args, state, control, **kwargs):
-        logs = kwargs['logs']
-        # ic(kwargs)
-        ic(args, state, control)
-        if state.is_local_process_zero:
-            print('in my logs', logs)
+class MyLoggingCallback(TrainerCallback):
+    """
+    Requires
+        - Tuple of (custom compute_loss log, internal training log, internal validation log) for each step
+            - Intended for coupled training and evaluation
+        - Accuracy as a metric is passed to `Trainer` and training metric computed in `compute_loss` and logged
+    """
+    def __init__(self, trainer: Trainer, name='HfLogging'):
+        """
+        :param trainer: The parent Trainer
+        :param name: Logger name
+        """
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler(stream=sys.stdout)  # For my own coloring
+        handler.setLevel(logging.DEBUG)
+        handler.setFormatter(MyFormatter())
+        self.logger.addHandler(handler)
 
-    def on_evaluate(self, args, state, control, logs=None, **kwargs):
-        print('in my evaluate')
+        # Heuristics: Expect
+        self.log_count = 0
+        # self.prev_log = None
+        self.out_dict: Dict = None
+        self.parent_trainer = trainer
+        self.called_val_init = False
+
+    def on_log(self, args, state, control, logs: Dict = None, **kwargs):
+        def out_dict2str(d: Dict):
+            # step_, n_ep__, tr_loss_, vl_loss_, tr_acc_, vl_acc_ = (
+            #     d.get(k, None) for k in ('step', 'epoch', 'train_loss', 'eval_loss', 'train_acc', 'eval_acc')
+            # )
+            # assert all(elm is not None for elm in (step_, tr_loss_, vl_loss_, tr_acc_, vl_acc_))
+            if 'epoch' in d:
+                keys_ = ('step', 'epoch', 'train_loss', 'eval_loss', 'train_acc', 'eval_acc')
+            else:
+                keys_ = ('step', 'train_loss', 'eval_loss', 'train_acc', 'eval_acc')
+            ic(d)
+            assert all(k in d for k in ('step', 'train_loss', 'eval_loss', 'train_acc', 'eval_acc'))
+            # s_out = 'step={step_}'
+            # if 'epoch' in d:
+            #     args_ = step_, n_ep__, tr_loss_, vl_loss_, tr_acc_, vl_acc_
+            # else:
+            #     # s_out += ', epoch={n_ep__}'
+            #     args_ = step_, tr_loss_, vl_loss_, tr_acc_, vl_acc_
+            # s_out += 'train_loss={tr_loss_}, eval_loss={vl_loss_}, train_acc={tr_acc_}, eval_acc={vl_acc_}'
+
+            d = {k: (('loss' in k and round(v, 4)) or ('acc' in k and round(v*100, 4)) or v) for k, v in d.items()}
+            s_fmt = ', '.join(f'{k}={{{k}}}' for k in keys_)
+            d = {k: logi(v) for k, v in d.items()}
+            # ic(s_fmt, d)
+            return s_fmt.format(**d)
+
+        if state.is_local_process_zero:
+            self.log_count += 1
+            ic(logs, state.global_step, state.epoch)
+            step = state.global_step
+            if 'src' in logs and logs['src'] == 'compute_loss':  # Custom added metric computation
+                # Before model runs, initial call
+                pass
+                if step == 0:
+                    if not self.called_val_init:  # Prevents circular logging call, see Trainer.evaluate()
+                        self.called_val_init = True
+                        # ic('should come here once only')
+                        tr_acc, tr_loss, n_ep = (logs.get(k, None) for k in ('acc', 'loss', 'epoch'))
+
+                        # Other `on_log` calls invoked inside `evaluate` ignored
+                        out: Dict = self.parent_trainer.evaluate()
+                        n_ep_, vl_acc, vl_loss = (out.get(k, None) for k in ('epoch', 'eval_accuracy', 'eval_loss'))
+                        assert all(elm is not None for elm in (n_ep, vl_acc, vl_loss))
+                        assert n_ep == n_ep_
+                        # Training step in range (1, total steps); TODO: epoch trouble some to calculate
+                        self.out_dict = dict(step=step, train_acc=tr_acc, train_loss=tr_loss)
+                        self.logger.info(out_dict2str(self.out_dict | dict(eval_acc=vl_acc, eval_loss=vl_loss)))
+                        # self.out_dict = None
+                else:  # Need to look for the accuracy calculated for the training batch
+                    # acc, loss = logs.get('acc', None), logs.get('loss', None)
+                    # assert acc is not None and loss is not None
+                    assert all(k in logs for k in ('acc', 'loss'))
+                    d_cand = logs | dict(step=step)
+                    ic('in non-edge compute_loss', self.out_dict, d_cand)
+                    if self.out_dict is None:  # Heuristic: 1st call to `computue_loss` corresponds to training
+                        # self.out_dict = dict(step=state.global_step, train_acc_cands=[acc], train_loss_cands=[loss])
+                        # ic('in starting new compute_loss cand', d_cand)
+                        self.out_dict = dict(candidates=[d_cand])
+                    else:
+                        pass
+                        # assert self.out_dict['step'] == state.global_step
+                        # self.out_dict['train_acc_cands'].append(acc)
+                        # self.out_dict['train_loss_cands'].append(loss)
+                        # ic('in adding compute_loss cand', self.out_dict, d_cand)
+                        # assert all(dc['step'] == step for dc in self.out_dict['candidates'])  # TODO
+                        # self.out_dict['candidates'].append(d_cand)  # TODO
+            elif 'loss' in logs:  # Internal training log
+                # Edge case step = 1: Before training start, i.e. step=1, stats for training already logged,
+                # But log anyway, for after gradient update, evaluation loss changes
+                # See Trainer.train(); compute_loss executes before step increments
+                # Without overriding `_maybe_log_save_evaluate`, can only get the training loss with 4 decimal place
+                tr_loss, lr, n_ep = (logs.get(k, None) for k in ('loss', 'learning_rate', 'epoch'))
+                assert all(elm is not None for elm in (tr_loss, lr, n_ep))
+                # hopefully eval loss candidates are not too close
+                # acc_cands, loss_cands = (
+                #     self.out_dict.get(k, None) for k in ('train_acc_cands', 'train_loss_cands')
+                # )
+                if step == 1:
+                    assert self.out_dict['step'] == step-1  # Override step
+                    self.out_dict.update(dict(step=step, train_loss=tr_loss, lr=lr, epoch=n_ep))
+                else:
+                    cands = self.out_dict['candidates']
+                    idx_train = min(range(len(cands)), key=lambda idx: abs(cands[idx]['loss']-tr_loss))
+                    d_tr = cands[idx_train]
+                    assert d_tr['step'] == step-1
+                    # Heuristics, the compute_loss for train seems to be at the end
+                    assert idx_train == len(cands)-1
+                    self.out_dict.update(dict(step=step, train_acc=d_tr['acc'], train_loss=tr_loss, lr=lr, epoch=n_ep))
+                    ic('in training log', self.out_dict)
+                    # assert 'train_acc_cands' in self.out_dict
+            else:
+                pass
+                # if step not in [0, 1]:  # Similarly, step is before training step increment
+                ic('in eval logging', step, self.out_dict)
+                if step != 0:
+                    assert 'eval_loss' in logs
+                    vl_loss, vl_acc, n_ep_ = (logs.get(k, None) for k in ('eval_loss', 'eval_accuracy', 'epoch'))
+                    assert all(elm is not None for elm in (vl_loss, vl_acc, n_ep_))
+                    assert step == self.out_dict['step']
+                    if step != 1:  # See `compute_loss` logging edge case above
+                        assert n_ep_ == self.out_dict['epoch']
+                    self.logger.info(out_dict2str(self.out_dict | dict(eval_loss=vl_loss, eval_acc=vl_acc)))
+                    self.out_dict = None
+                else:  # Skip printing
+                    self.out_dict = None
+
+            # ic(logs, state, control)
+            # if self.log_count == 1:
+            #     # assert 'train_acc' in logs and 'epoch' in logs
+            #     self.out_dict = logs  # Resets
+            #     self.out_dict['step'] = state.global_step
+            # elif self.log_count == 2:
+            #     tr_loss, lr, n_ep = (logs.get(k, None) for k in ('loss', 'learning_rate', 'epoch'))
+            #     # assert all(elm is not None for elm in (tr_loss, lr, n_ep))
+            #     # assert n_ep == self.out_dict['epoch']
+            #     self.out_dict['learning_rate'] = lr
+            #     self.out_dict['train_loss'] = tr_loss
+            # elif self.log_count == 3:  # Take cached result, log, and rest
+            #     vl_loss, vl_acc, n_ep = (logs.get(k, None) for k in ('eval_loss', 'eval_accuracy', 'epoch'))
+            #     # assert all(elm is not None for elm in (vl_loss, vl_acc, n_ep))
+            #     # assert n_ep == self.out_dict['epoch']
+            #     self.out_dict['eval_loss'] = vl_loss
+            #     self.out_dict['eval_acc'] = vl_acc
+            #
+            #     # self.logger.info(self.out_dict)
+            #     self.log_count = 0
+            # s = logs
+            # ic(control)
 
 
 class CustomTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # assert 'args' in kwargs and 'callbacks' in kwargs
+        # Expect a custom logging callback passed in to **replace** the internal callback
+        callbacks = self.callback_handler.callbacks
+        # ic(callbacks)
+        # ic([str(c.__class__) for c in callbacks])
+        # ic([
+        #     str(c.__class__) != "<class 'transformers.trainer_callback.PrinterCallback'>" for c in callbacks
+        # ])
+        # ic([
+        #     c for c in callbacks if str(c.__class__) != "<class 'transformers.trainer_callback.PrinterCallback'>"
+        # ])
+        self.callback_handler.callbacks = [
+            c for c in callbacks if str(c.__class__) != "<class 'transformers.trainer_callback.PrinterCallback'>"
+        ]
+        ic(self.callback_handler.callbacks)
+        # exit(1)
+
     def compute_loss(self, model, inputs, return_outputs=False):
         """
         Override `Trainer.compute_loss` for logging accuracy
+            - Note that both training and validation calls `compute_loss`
+                => Further logic needs to determine accuracy for which dataset
 
         Modified from https://discuss.huggingface.co/t/metrics-for-training-set-in-trainer/2461/4?u=stefanh
         """
@@ -135,12 +303,13 @@ class CustomTrainer(Trainer):
             labels = None
         outputs = model(**inputs)
 
-        # ========================== Added ==========================
+        # ========================== Begin of added ==========================
         if 'labels' in inputs:
             preds = outputs.logits.detach()
             matches: torch.Tensor = (preds.argmax(axis=-1) == inputs['labels'])
-            self.log(dict(training_accuracy=round((matches.sum() / matches.numel()).item(), 4)))
-            # ic(dict(training_accuracy=round((matches.sum() / matches.numel()).item(), 4)))
+            # ic('in compute loss', list(outputs.keys()))
+            log_dict = dict(src='compute_loss', acc=round((matches.sum() / matches.numel()).item(), 4))
+            # self.log(log_dict)
         # ========================== End of added ==========================
 
         # Save past state if it exists
@@ -155,7 +324,26 @@ class CustomTrainer(Trainer):
             # ModelOutput.
             loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
 
+        # ========================== Begin of added ==========================
+        if 'labels' in inputs:
+            log_dict['loss'] = loss.detach().item()  # For determining which dataset
+            # ic('in compute loss', log_dict)
+            self.log(log_dict)
+        # ========================== End of added ==========================
+
         return (loss, outputs) if return_outputs else loss
+
+    # def log(self, logs: Dict[str, float]) -> None:
+    #     if self.state.epoch is not None:
+    #         logs["epoch"] = round(self.state.epoch, 2)
+    #
+    #     output = {**logs, **{"step": self.state.global_step}}
+    #     self.state.log_history.append(output)
+    #     self.control = self.callback_handler.on_log(self.args, self.state, self.control, logs)
+    #
+    #     # ========================== Begin of added ==========================
+    #
+    #     # ========================== End of added ==========================
 
 
 if __name__ == '__main__':
@@ -169,7 +357,7 @@ if __name__ == '__main__':
     nm = 'debug'
     model, tokenizer = get_model_n_tokenizer(nm)
     train_args, data_collator = get_train_setup(nm)
-    ic(train_args)
+    # ic(train_args)
 
     def tokenize_func(sample):
         ret = tokenizer(sample['text'], padding='max_length', truncation=True)
@@ -183,10 +371,7 @@ if __name__ == '__main__':
         logits, labels = eval_pred
         predictions = np.argmax(logits, axis=-1)
         labels, predictions = labels.flatten(), predictions.flatten()  # Original 2D tensor gives error
-        # ic(logits, labels, predictions, labels.shape, predictions.shape)
         return metric.compute(predictions=predictions, references=labels)
-
-    cb = PrinterCallback()
 
     # trainer = Trainer(
     trainer = CustomTrainer(
@@ -196,8 +381,9 @@ if __name__ == '__main__':
         # data_collator=default_data_collator,
         train_dataset=dset_tr,
         eval_dataset=dset_vl,
-        compute_metrics=compute_metrics,
-        # callbacks=[cb]
+        compute_metrics=compute_metrics
     )
+    cb = MyLoggingCallback(trainer)
+    trainer.add_callback(cb)
     trainer.train()
     ic(trainer.evaluate())
--- unified_encoder/util/util.py
+++ unified_encoder/util/util.py
@@ -1,16 +1,20 @@
+import re
 import sys
 import json
 import math
+import logging
 from typing import Union
-from functools import reduce
 from datetime import datetime
+from functools import reduce
+from collections import OrderedDict
 
 import numpy as np
 import torch
 from matplotlib import rcParams
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import colorama
+import sty
 
 from .data_path import *
 
@@ -71,6 +75,156 @@ def config(attr):
 def now(as_str=True):
     d = datetime.now()
     return d.strftime('%Y-%m-%d %H:%M:%S') if as_str else d
+
+
+def log(s, c: str = 'log', c_time='green', as_str=False):
+    """
+    Prints `s` to console with color `c`
+    """
+    if not hasattr(log, 'reset'):
+        log.reset = colorama.Fore.RESET + colorama.Back.RESET + colorama.Style.RESET_ALL
+    if not hasattr(log, 'd'):
+        log.d = dict(
+            log='',
+            warn=colorama.Fore.YELLOW,
+            error=colorama.Fore.RED,
+            err=colorama.Fore.RED,
+            success=colorama.Fore.GREEN,
+            suc=colorama.Fore.GREEN,
+            info=colorama.Fore.BLUE,
+            i=colorama.Fore.BLUE,
+            w=colorama.Fore.RED,
+
+            y=colorama.Fore.YELLOW,
+            yellow=colorama.Fore.YELLOW,
+            red=colorama.Fore.RED,
+            r=colorama.Fore.RED,
+            green=colorama.Fore.GREEN,
+            g=colorama.Fore.GREEN,
+            blue=colorama.Fore.BLUE,
+            b=colorama.Fore.BLUE,
+
+            m=colorama.Fore.MAGENTA
+        )
+    if c in log.d:
+        c = log.d[c]
+    if as_str:
+        return f'{c}{s}{log.reset}'
+    else:
+        print(f'{c}{log(now(), c=c_time, as_str=True)}| {s}{log.reset}')
+
+
+def logs(s, c):
+    return log(s, c=c, as_str=True)
+
+
+def logi(s):
+    """
+    Syntactic sugar for logging `info` as string
+    """
+    return logs(s, c='i')
+
+
+def hex2rgb(hx: str) -> Union[tuple[int], tuple[float]]:
+    # Modified from https://stackoverflow.com/a/62083599/10732321
+    if not hasattr(hex2rgb, 'regex'):
+        hex2rgb.regex = re.compile(r'#[a-fA-F0-9]{3}(?:[a-fA-F0-9]{3})?$')
+    m = hex2rgb.regex.match(hx)
+    assert m is not None
+    if len(hx) <= 4:
+        return tuple(int(hx[i]*2, 16) for i in range(1, 4))
+    else:
+        return tuple(int(hx[i:i+2], 16) for i in range(1, 7, 2))
+
+
+class MyTheme:
+    """
+    Theme based on `sty` and `Atom OneDark`
+    """
+    COLORS = OrderedDict([
+        ('yellow', 'E5C07B'),
+        ('green', '00BA8E'),
+        ('blue', '61AFEF'),
+        ('cyan', '2AA198'),
+        ('red', 'E06C75'),
+        ('purple', 'C678DD')
+    ])
+    yellow, green, blue, cyan, red, purple = (
+        hex2rgb(f'#{h}') for h in ['E5C07B', '00BA8E', '61AFEF', '2AA198', 'E06C75', 'C678DD']
+    )
+
+    @staticmethod
+    def set_color_type(t: str):
+        """
+        Sets the class attribute accordingly
+
+        :param t: One of ['rgb`, `sty`]
+            If `rgb`: 3-tuple of rgb values
+            If `sty`: String for terminal styling prefix
+        """
+        for color, hex_ in MyTheme.COLORS.items():
+            val = hex2rgb(f'#{hex_}')  # For `rgb`
+            if t == 'sty':
+                setattr(sty.fg, color, sty.Style(sty.RgbFg(*val)))
+                val = getattr(sty.fg, color)
+            setattr(MyTheme, color, val)
+
+
+class MyFormatter(logging.Formatter):
+    """
+    Modified from https://stackoverflow.com/a/56944256/10732321
+
+    Default styling: Time in green, metadata indicates severity, plain log message
+    """
+    RESET = sty.rs.fg + sty.rs.bg + sty.rs.ef
+
+    MyTheme.set_color_type('sty')
+    yellow, green, blue, cyan, red, purple = (
+        MyTheme.yellow, MyTheme.green, MyTheme.blue, MyTheme.cyan, MyTheme.red, MyTheme.purple
+    )
+
+    KW_TIME = '%(asctime)s'
+    KW_MSG = '%(message)s'
+    KW_LINENO = '%(lineno)d'
+    KW_FNM = '%(filename)s'
+    KW_FUNCNM = '%(funcName)s'
+    KW_NAME = '%(name)s'
+
+    DEBUG = INFO = BASE = RESET
+    WARN, ERR, CRIT = yellow, red, purple
+    CRIT += sty.Style(sty.ef.bold)
+
+    LVL_MAP = {  # level => (abbreviation, style)
+        logging.DEBUG: ('DBG', DEBUG),
+        logging.INFO: ('INFO', INFO),
+        logging.WARNING: ('WARN', WARN),
+        logging.ERROR: ('ERR', ERR),
+        logging.CRITICAL: ('CRIT', CRIT)
+    }
+
+    def __init__(self, fmt_time=green):
+        super().__init__()
+
+        sty_kw, reset = MyFormatter.blue, MyFormatter.RESET
+        fmt_time = f'{fmt_time}{MyFormatter.KW_TIME}{sty_kw}| {reset}'
+
+        def fmt_meta(meta_abv, meta_style):
+            return f'{MyFormatter.purple}[{MyFormatter.KW_NAME}]' \
+                   f'{MyFormatter.blue}::{MyFormatter.purple}{MyFormatter.KW_FUNCNM}' \
+                   f'{MyFormatter.blue}::{MyFormatter.purple}{MyFormatter.KW_FNM}' \
+                   f'{MyFormatter.blue}:{MyFormatter.purple}{MyFormatter.KW_LINENO}' \
+                   f'{MyFormatter.blue}, {meta_style}{meta_abv}{reset}'
+
+        self.formats = {
+            level: fmt_time + fmt_meta(*args) + f'{sty_kw} - {reset}{MyFormatter.KW_MSG}' + reset
+            for level, args in MyFormatter.LVL_MAP.items()
+        }
+        self.formatter = {
+            lv: logging.Formatter(fmt, datefmt='%Y-%m-%d %H:%M:%S') for lv, fmt in self.formats.items()
+        }
+
+    def format(self, entry):
+        return self.formatter[entry.levelno].format(entry)
 
 
 def fmt_num(n: Union[float, int]):
