c2686a1e4a86e1baeb0372330fab6aaa12ccd852
Stefan Heng <stefan.hg@outlook.com>
2022-06-03 15:24:45 -0400

    New: training aspect sampling

--- zeroshot_encoder/util/load_data.py
+++ zeroshot_encoder/util/load_data.py
@@ -8,8 +8,8 @@ import itertools
 from os import listdir
 from os.path import isfile, join, basename
 from zipfile import ZipFile
-from collections import Counter
-from typing import List, Tuple, Dict
+from collections import Counter, defaultdict
+from typing import List, Tuple, Set, Dict
 
 import numpy as np
 from numpy import argmax, argmin
@@ -53,11 +53,13 @@ category_map = {
 }
 
 
-def get_data(path, n_sample: int = None):
+def get_data(path: str, n_sample: int = None, normalize_aspect: bool = False) -> Dict[str, Dict]:
     """
     :param path: File system path to folder of UTCD dataset
     :param n_sample: If given, a random sample of the entire dataset is selected
         Intended for debugging
+    :param normalize_aspect: If true, # of training samples for each aspect is normalized
+        via subsampling datasets in the larger aspect
     """
     logger = get_logger('Get UTCD data')
     if not os.path.exists(path):
@@ -69,6 +71,7 @@ def get_data(path, n_sample: int = None):
         dataset_name = basename(path).split('.')[0]
         logger.info(f'Loading dataset {logi(dataset_name)}...')
         dset = json.load(open(path))
+
         if n_sample:
             assert set(dset.keys()) == {'train', 'test', 'aspect', 'labels'}  # sanity check
             for k in ['train', 'test']:
@@ -79,7 +82,73 @@ def get_data(path, n_sample: int = None):
                 txts = np.random.permutation(txts)[:n_sample]
                 dset[k] = {t: txt2lb[t] for t in txts}
         data[dataset_name] = dset
+    if normalize_aspect:
+        data = sample_data(data)
     return data
+
+
+def sample_data(data: Dict[str, Dict]) -> Dict[str, Dict]:
+    """
+    Sample the `train` split of the 9 in-domain datasets so that each `aspect` contains same # of samples
+
+    Maintain class distribution
+    """
+    aspect2n_txt = defaultdict(int)
+    for dnm, d_dset in sconfig('UTCD.datasets').items():
+        if d_dset['domain'] == 'in':
+            aspect2n_txt[d_dset['aspect']] += d_dset['splits']['train']['n_text']
+    ic(aspect2n_txt)
+    asp_min = min(aspect2n_txt, key=aspect2n_txt.get)
+    asps_sample = [asp for asp in aspect2n_txt.keys() if asp != asp_min]
+    ic(asp_min, asps_sample)
+    # exit(1)
+
+    def sample_dset(d: Dict[str, List[str]], dnm_: str, n_text: int) -> Dict[str, List[str]]:
+        """
+        Sample texts from text-labels pairs to `n_sample` while maintaining class distribution
+        """
+        # cls2count = Counter()
+        cls2txt: Dict[str, Set[str]] = defaultdict(set)
+        for txt, lbs in d.items():
+            # cls2count.update(lbs)
+            for lb in lbs:  # the same text may be added to multiple classes & hence sampled multiple times, see below
+                cls2txt[lb].add(txt)
+        cls2count = {cls: len(txts) for cls, txts in cls2txt.items()}
+        # ic(cls2count, sum(cls2count.values()))
+        # normalize by #pair instead of #text for keeping output #text close to `n_sample`
+        ratio = n_text / sconfig(f'UTCD.datasets.{dnm_}.splits.train.n_pair')
+        cls2count = {cls: round(c * ratio) for cls, c in cls2count.items()}  # goal count for output
+        # ic(cls2count, sum(cls2count.values()))
+        out = dict()
+        for cls, c in cls2count.items():
+            to_sample = c
+            while to_sample > 0:
+                txts = random.sample(cls2txt[cls], to_sample)
+                for t in txts:
+                    if t not in out:  # ensure no-duplication in # samples added, since multi-label
+                        out[t] = d[t]
+                        to_sample -= 1
+                        cls2txt[cls].remove(t)
+        return out
+
+    for dnm, d_dset in data.items():
+        asp = sconfig(f'UTCD.datasets.{dnm}.aspect')
+        if asp != asp_min:
+            # ic(dnm)
+            n_normed = sconfig(f'UTCD.datasets.{dnm}.splits.train.n_text') * aspect2n_txt[asp_min] / aspect2n_txt[asp]
+            # ic(sconfig(f'UTCD.datasets.{dnm}.splits.train.n_text'), n_normed)
+            d_dset['train'] = sample_dset(d_dset['train'], dnm, n_text=round(n_normed))
+            # ic(dnm, len(dset), sum([len(lbs) for lbs in dset.values()]))
+            # exit(1)
+    return data
+
+    # for dnm, dset in data.items():
+    #     ic(dnm)
+    #     c = Counter()
+    #     for txt, lbs in dset['train'].items():
+    #         c.update(lbs)
+    #     ic(c, len(c))
+        # exit(1)
 
 
 def download_data(path, file=None):
@@ -414,3 +483,20 @@ def binary_explicit_format(dataset):
                 elif len(other_labels) > 0:
                     train.append(ExplicitInputExample(texts=[text, other_labels[0]], label=0, aspect=aspect_map[aspect]))
     return train
+
+
+if __name__ == '__main__':
+    from icecream import ic
+
+    ic.lineWrapWidth = 512
+
+    random.seed(sconfig('random-seed'))
+
+    def check_sampling():
+        data = get_data(in_domain_data_path)
+        # ic(data_)
+        data = sample_data(data)
+        # ic(data)
+        for dnm, d_dset in data.items():
+            ic(dnm, len(d_dset['train']))
+    check_sampling()
