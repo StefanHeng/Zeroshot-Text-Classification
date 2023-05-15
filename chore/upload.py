3506279548cb041d71a45637a40f719a5fb9a452
Stefan Heng <stefan.hg@outlook.com>
2023-05-15 12:25:09 -0400

    New: model upload to HF working

--- chore/upload.py
+++ chore/upload.py
@@ -11,28 +11,26 @@ from tqdm.auto import tqdm
 
 from stefutil import *
 from zeroshot_classifier.util import *
+from chore.util import *
 
 
-def save_each_split(dir_nm):
+def save_each_dataset_split(dir_nm):
     """
     :param dir_nm: directory name containing datasets to split into separate files by dataset split
     """
     path = os_join(u.proj_path, u.dset_dir, dir_nm)
     assert os.path.exists(path)  # sanity check
     path_out = os_join(u.proj_path, u.dset_dir, f'{dir_nm}_split')
-    # mic(path_out)
     assert not os.path.exists(path_out)
     os.mkdir(path_out)
 
     paths = list(glob.iglob(os_join(path, '*.json')))
-    # mic(list(paths))
     it = tqdm(paths, desc=f'Saving each split on {dir_nm}')
     for path in it:
         dnm = stem(path)
         it.set_postfix(dnm=pl.i(dnm))
         with open(path) as f:
             d = json.load(f)
-        # mic(d.keys())
         assert set(d.keys()).issubset({'train', 'eval', 'test', 'labels', 'aspect'})
 
         path_out_dset = os_join(path_out, dnm)
@@ -41,11 +39,83 @@ def save_each_split(dir_nm):
         for split in splits:
             with open(os_join(path_out_dset, f'{split}.json'), 'w') as f:
                 json.dump(d[split], f)
-        # raise NotImplementedError
 
 
 if __name__ == '__main__':
-    # save_each_split('in-domain')
-    # save_each_split('out-of-domain')
-    # save_each_split('aspect-normalized-in-domain')
-    save_each_split('aspect-normalized-out-of-domain')
+    import pandas as pd
+
+    def save_dset_split():
+        # save_each_dataset_split('in-domain')
+        # save_each_dataset_split('out-of-domain')
+        # save_each_dataset_split('aspect-normalized-in-domain')
+        save_each_dataset_split('aspect-normalized-out-of-domain')
+    # save_dset_split()
+
+    def check_previous_model_performance():
+        dom2dnms = cconfig('domain2dataset-names-all')
+        dnms_in, dnms_out = dom2dnms['in'], dom2dnms['out']
+        mic(dnms_in, dnms_out)
+
+        path = os_join(u.eval_path, 'model-upload')
+
+        d = dict()
+        for md_nm in ['binary-bert', 'bi-encoder']:
+            for strat in ['vanilla', 'implicit-sep', 'explicit']:
+                dir_nm = f'{md_nm}-{strat}'
+                path_dnm = os_join(path, dir_nm)
+                paths = os.listdir(path_dnm)
+                paths_in = [p for p in paths if p.startswith('in-domain')]
+                paths_out = [p for p in paths if p.startswith('out-of-domain')]
+                assert len(paths_in) == len(paths_out) == 1
+
+                dom = 'out'
+                path_in = os_join(path_dnm, paths_in[0])
+                path_out = os_join(path_dnm, paths_out[0])
+                # mic(path_in, path_out)
+
+                fnms = os.listdir(path_in) if dom == 'in' else os.listdir(path_out)
+                accs = []
+                for dnm in (dnms_in if dom == 'in' else dnms_out):
+                    dnm_ = f'{dnm}.csv'
+                    assert dnm_ in fnms
+                    df = pd.read_csv(os_join((path_in if dom == 'in' else path_out), dnm_))
+                    df = df.iloc[-3:, :].reset_index(drop=True)  # only keep the aggregated rows
+                    accs.append(df.loc[0, 'f1-score'])  # f1-score from accuracy row
+                d[dir_nm] = sum(accs) / len(accs)
+                # raise NotImplementedError
+        mic(d)
+    # check_previous_model_performance()
+
+    org_nm = 'claritylab'
+
+    def upload_models():
+        from zeroshot_classifier.models.architecture import BinaryBertCrossEncoder
+        from transformers import BertForSequenceClassification, TFBertForSequenceClassification
+
+        # md_nm = '06.03.22_binary-bert-asp-norm-vanilla'
+        # md_nm = '06.03.22_binary-bert-asp-norm-implicit-sep'
+        md_nm = '06.04.22_binary-bert-explicit-finetune'
+        model_path = os_join(u.base_path, 'models-upload', md_nm)
+        mic(model_path)
+        assert os.path.exists(model_path)  # sanity check
+        # mic(os.listdir(model_path))
+
+        model = BinaryBertCrossEncoder(model_name=model_path)
+        model, tokenizer = model.model, model.tokenizer
+        assert isinstance(model, BertForSequenceClassification)  # sanity check
+        model_tf = TFBertForSequenceClassification.from_pretrained(model_path, from_pt=True)
+        mic(type(model), type(model_tf), tokenizer)
+
+        # md_nm_hf = 'zero-shot-vanilla-binary-bert'
+        # md_nm_hf = 'zero-shot-implicit-binary-bert'
+        md_nm_hf = 'zero-shot-explicit-binary-bert'
+        # `sentence-transformers` uses huggingface-hub 0.4.0, uploading to hub w/ org code different
+        repo_id = f'{org_nm}/{md_nm_hf}'
+        mic(repo_id)
+        # model.push_to_hub(repo_id)
+        # model_tf.push_to_hub(repo_id)
+        # tokenizer.push_to_hub(repo_id)
+        model.push_to_hub(repo_path_or_name=md_nm_hf, organization=org_nm)
+        model_tf.push_to_hub(repo_path_or_name=md_nm_hf, organization=org_nm)
+        tokenizer.push_to_hub(repo_path_or_name=md_nm_hf, organization=org_nm)
+    upload_models()
