e705357f87e4b0b19bced008519b264b5928b516
Stefan Heng <stefan.hg@outlook.com>
2023-05-17 12:44:18 -0400

    Fix: HF bert models imp sep

--- chore/upload_readme.py
+++ chore/upload_readme.py
@@ -6,13 +6,17 @@ Make sure standalone code for running inference on uploaded models work
 if __name__ == '__main__':
     def binary_bert_instr():
         from zeroshot_classifier.models import BinaryBertCrossEncoder
-        model = BinaryBertCrossEncoder(model_name='claritylab/zero-shot-vanilla-binary-bert')
+        model = BinaryBertCrossEncoder(model_name='claritylab/zero-shot-implicit-binary-bert')
 
         text = "I'd like to have this track onto my Classical Relaxations playlist."
         labels = [
             'Add To Playlist', 'Book Restaurant', 'Get Weather', 'Play Music', 'Rate Book', 'Search Creative Work',
             'Search Screening Event'
         ]
+        aspect = 'intent'
+        sep_token = '<|ASPECT-SEP|>'
+        text = f'{aspect} {sep_token} {text}'
+
         query = [[text, lb] for lb in labels]
         logits = model.predict(query, apply_softmax=True)
         print(logits)
@@ -21,13 +25,17 @@ if __name__ == '__main__':
     def bi_encoder_instr():
         from sentence_transformers import SentenceTransformer, util as sbert_util
 
-        model = SentenceTransformer(model_name_or_path='claritylab/zero-shot-implicit-bi-encoder')
+        model = SentenceTransformer(model_name_or_path='claritylab/zero-shot-explicit-bi-encoder')
 
         text = "I'd like to have this track onto my Classical Relaxations playlist."
         labels = [
             'Add To Playlist', 'Book Restaurant', 'Get Weather', 'Play Music', 'Rate Book', 'Search Creative Work',
             'Search Screening Event'
         ]
+        # aspect = 'intent'
+        # sep_token = '<|ASPECT-SEP|>'
+        # text = f'{aspect} {sep_token} {text}'
+
         text_embed = model.encode(text)
         label_embeds = model.encode(labels)
 
@@ -37,28 +45,24 @@ if __name__ == '__main__':
 
     def gpt2_instr():
         import torch
-
         from zeroshot_classifier.models import ZsGPT2Tokenizer, ZsGPT2LMHeadModel
 
-        model_name = 'claritylab/zero-shot-vanilla-gpt2'
+        training_strategy = 'vanilla'
+        model_name = f'claritylab/zero-shot-{training_strategy}-gpt2'
         model = ZsGPT2LMHeadModel.from_pretrained(model_name)
-        tokenizer = ZsGPT2Tokenizer.from_pretrained(model_name, form='vanilla')
+        tokenizer = ZsGPT2Tokenizer.from_pretrained(model_name, form=training_strategy)
 
-        model.config.pad_token_id = model.config.eos_token_id
-        model.eval()
-
-        # 'dataset_name` just so that it passes, irrelevant
         text = "I'd like to have this track onto my Classical Relaxations playlist."
         labels = [
             'Add To Playlist', 'Book Restaurant', 'Get Weather', 'Play Music', 'Rate Book', 'Search Creative Work',
             'Search Screening Event'
         ]
+
         inputs = tokenizer(
             dict(text=text, label_options=labels), mode='inference-sample'
         )
-
         inputs = {k: torch.tensor(v).unsqueeze(0) for k, v in inputs.items()}
         outputs = model.generate(**inputs, max_length=128)
-        decoded = tokenizer.batch_decode(outputs, skip_special_tokens=False)
+        decoded = tokenizer.batch_decode(outputs, skip_special_tokens=False)[0]
         print(decoded)
     gpt2_instr()
